413702674c690507262c80df8088a834d517a609
berkbbd <berk@bbdtech.co>
2024-01-23 15:59:52 +0300

    welcome page improved.

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -7,14 +7,12 @@ const Welcome = () => {
     const navigate = useNavigate();
     return (
         <div className="top-container">
-            <div className="container-wrapper">
-                <div className="auth-container">
-                    <h2 className="title">Welcome to EventFlow</h2>
-                    <div>Manage your conference program easily.</div>
-                    <div className="button-container">
-                        <button onClick={() => navigate(RoutesConfig.createPresentation)}>Create Presentation</button>
-                        <button onClick={() => navigate(RoutesConfig.conferenceAgenda)}>Conference Agenda</button>
-                    </div>
+            <div className="auth-container">
+                <h2 className="title">Welcome to EventFlow</h2>
+                <div>Manage your conference program easily.</div>
+                <div className="button-container">
+                    <button onClick={() => navigate(RoutesConfig.createPresentation)}>Create Presentation</button>
+                    <button onClick={() => navigate(RoutesConfig.conferenceAgenda)}>Conference Agenda</button>
                 </div>
             </div>
         </div>
